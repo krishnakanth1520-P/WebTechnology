77b9b870743bafcec62790dfdf82f3e5fa8de7d2
krishnakanth1520-P <krishnkanth1520@gmail.com>
2026-01-09 10:33:39 +0530

    Webtech files

---
+++ JAVASCRIPT/4_Hoisting/hoisting.js
@@ -0,0 +1,16 @@
+console.log("Hiii");
+
+console.log("Helloo");
+
+console.log("Byeee");
+
+let a=10;
+let b=20;
+
+console.log(a);
+
+hoist();
+
+function hoist(){
+    console.log("Inside hoist function");
+}
---
+++ JAVASCRIPT/5_Function/function.js
@@ -0,0 +1,44 @@
+function anyName(){
+    console.log("Jana nayagan Kandipa nalaiku varatha")
+}
+
+anyName()
+
+
+//With parametersfunction
+function add(a,b){
+    console.log(a);
+    console.log(b);
+    console.log(a+b);
+    
+
+
+}
+
+add(10,20) //calling the function with arguments
+
+
+function mul(a,b,c){
+    console.log(a*b*c);
+    
+}
+
+mul(10,20,30)
+
+
+
+
+
+function add(a,b){
+    console.log("hello");
+    return a+b
+}
+// let addresult=add       //represent the entire function and function will not be called
+let addresult=add(30,50)   //Return value of the function
+console.log(addresult);
+
+
+
+
+
+    
